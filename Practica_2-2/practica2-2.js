b43e4a56fbeaee6934352a02901fb79ee943d0fe
rogeriesmontsia <rogerarques@iesmontsia.org>
2023-09-23 15:05:50 +0200

    estils i funcions

--- Practica_2-2/practica2-2.js
+++ Practica_2-2/practica2-2.js
@@ -1,50 +1,66 @@
-$(document).ready(function() {
-    $('#formulari').hide()
-    $('#mostrardades').hide()
-});
-    function formulari1() {
-        var email = $('#email').val();
-        var password = $('#pwd').val();
-        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        
-        if ($('#email').val() === '' || $('#pwd').val() === '') {
-            $('#email').css('border', '2px solid red')
-            $('#pwd').css('border', '2px solid red')
-            $('#error').text('Completa tots els camps').show();
+$(document).ready(function () {
+    $('#formulari').hide();
+    $('#mostrardades').hide();
+})
 
-        }else if (!email.match(mailformat)){
-            $('#error').text('Introdueix un correu en format correcte.').show();
-            
-        }
-        else {
-            $('#login').fadeOut(3000)
-            $('#formulari').fadeIn(3000)
-        } 
-    };
-    function formulari2() {
+let email, password, mailformat, nom, cognoms, edat;
 
-        // Obtén los valores de los campos
-        var nom = $('#nom').val()
-        var cognoms = $('#cognoms').val()
-        var edat = $('#edat').val()
-        var email = $('#email').val()
-        var password = $('#pwd').val()
+function dades() {
+    email = $('#email').val();
+    password = $('#pwd').val();
+    mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    nom = $('#nom').val();
+    nom = nom.charAt(0).toUpperCase() + nom.slice(1);
+    cognoms = $('#cognoms').val();
+    cognoms = cognoms.charAt(0).toUpperCase() + cognoms.slice(1);
+    edat = $('#edat').val();
+}
 
-        // Verifica si el camps estan plens
-        if (nom === '' || cognoms === '' || edat === '') {
-            $('#error2').text('Completa els camps si us plau.').show();
-        } else {
-            $('#formulari').hide()
-            // Construye el mensaje a mostrar
-            var mensaje = 'Perfil:\n' +
-              'E-mail: ' + email + '\n' +
-              'Password: ' + password + '\n' +
-              'Nombre: ' + nom + '\n' +
-              'Apellidos: ' + cognoms + '\n' +
-              'Edad: ' + edat;
+function comprovarDades1() {
+    if (email === '' && password === '') {
+        $('#email, #pwd').css('border', '2px solid red');
+        $('#error').text('Completa tots els camps').show();
 
-            // Muestra el mensaje en el área 'mostrardades'
-            $('#mostrardades').html('<pre>' + mensaje + '</pre>').show();
-        } 
-
-    };
+    } if (email === '' && password != '') {
+        $('#pwd').css('border', '');
+        $('#email').css('border', '2px solid red');
+        $('#error').text('Completa tots els camps').show();
+    } if (email != '' && !email.match(mailformat) && password != '') {
+        $('#error').text('Introdueix un mail en format correcte').show();
+    } if (email.match(mailformat) && password != '') {
+        $('#login').fadeOut(350);
+        $('#formulari').fadeIn(499);
+    }
+}
+function formulari1() {
+    dades();
+    comprovarDades1();
+}
+function comprovarDades2() {
+    if (nom === '' || cognoms === '' || edat === '') {
+        $('#error2').text('Completa els camps si us plau.').show();
+        $('#nom, #cognoms, #edat').css('border', '2px solid red');
+    } if (nom != '' && cognoms === '' && edat === '') {
+        $('#error2').text('Completa els camps si us plau.').show();
+        $('#nom').css('border', '');
+        $('#cognoms, #edat').css('border', '2px solid red');
+    } if (nom != '' && cognoms != '' && edat === '') {
+        $('#error2').text('Completa els camps si us plau.').show();
+        $('#nom, #cognoms').css('border', '');
+        $('#edat').css('border', '2px solid red');
+    } if (edat <= 0 && cognoms != '' && edat != '') {
+        $('#error2').text('Introdueix una edat valida.').show();
+    } if (nom != '' && cognoms != '' && edat > 0) {
+        $('#formulari').hide()
+        $('#mostrardades').show()
+        $('#mostrarnom span').text(nom).show();
+        $('#mostrarcognom span').text(cognoms).show();
+        $('#mostraredat span').text(edat).show();
+        $('#mostraremail span').text(email).show();
+        $('#mostrarpass span').text(password).show();
+    }
+}
+function formulari2() {
+    dades();
+    comprovarDades2();
+}
